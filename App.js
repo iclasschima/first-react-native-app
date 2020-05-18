8060ef739f9dee9585d59c37a78e3200446cf6ea
iClass <iclasschima@gmail.com>
2020-05-18 11:09:25 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+
+  const [state, setState] = useState("Hello world 😃")
+
+  const handleChange = async e => {
+    setState(e.target.value)
+
+    if (state.search("happy") !== -1) setState(state.replace("happy", "😃 "))
+    
+    if (state.search("sad") !== -1) setState(state.replace("sad", "😞 "))
+
+    if (state.search("love") !== -1) setState(state.replace("love", "💖 "))
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <TextInput
+        placeholder="Type here..."
+        onChange={ handleChange}
+        value={state}
+        style={{
+          height: "30px",
+          padding: "1rem",
+          marginBottom: "1rem",
+          borderColor: "grey",
+          borderWidth: 1
+        }}
+      />
+      <Button onPress={() => setState("")} title="Clear Text" />
     </View>
   );
 }
