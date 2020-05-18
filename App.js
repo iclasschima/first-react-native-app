69096ac7ab21ac6707750cc4cb6e83c4da75d78d
iClass <iclasschima@gmail.com>
2020-05-19 00:49:00 +0100

    working with sectionList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, SectionList } from 'react-native';
 
 export default function App() {
 
@@ -27,6 +27,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
+      <Text style={{fontSize: "17px", fontWeight: "bold", marginTop: "2rem", marginBottom: "1rem"}}>Flat List</Text>
         <FlatList
           style={{marginBottom: "20px"}}
           data={[
@@ -49,6 +50,19 @@ export default function App() {
         }}
       />
       <Button onPress={() => addToList()} title="Add" />
+
+        <Text style={{fontSize: "17px", fontWeight: "bold", marginTop: "2rem", marginBottom: "1rem"}}>Section List</Text>
+      <SectionList 
+        style={styles.section}
+        sections={[
+          {title: "Girls", data: ["Ella", "Ada", "Helen"]},
+          {title: "Boys", data: ["iClass", "Chima", "IC"]}
+        ]}
+
+        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+        keyExtractor={(item, index) => index}
+      />
     </View>
   );
 }
@@ -57,7 +71,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
+
+  sectionHeader: {
+    padding: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)'
+  },
+
+  item : {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+
+  section: {
+
+  }
 });
